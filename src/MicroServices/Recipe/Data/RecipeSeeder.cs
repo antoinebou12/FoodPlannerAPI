ecd7c25ee59e882cb31e385943387efcc571514e
Antoine Boucher <antoine.bou13@gmail.com>
2023-05-16 10:36:31 -0400

    Update RecipeSeeder.cs

--- src/MicroServices/Recipe/Data/RecipeSeeder.cs
+++ src/MicroServices/Recipe/Data/RecipeSeeder.cs
@@ -11,31 +11,24 @@ public class RecipeSeeder
         _context = context;
     }
 
-    public async Task SeedData(string path)
+    public async Task SeedData(string recipePath, string ingredientPath, string instructionPath, string nutritionInfoPath, string tagPath)
     {
-        using var reader = new StreamReader(path);
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-
-        var records = csv.GetRecords<Recipe>().ToList();
-
-        foreach (var record in records)
-        {
-            _context.Recipes.Add(record);
-        }
+        await ImportFromCsv(recipePath, csv => csv.GetRecords<Recipe>());
+        await ImportFromCsv(ingredientPath, csv => csv.GetRecords<Ingredient>());
+        await ImportFromCsv(instructionPath, csv => csv.GetRecords<Instruction>());
+        await ImportFromCsv(nutritionInfoPath, csv => csv.GetRecords<NutritionInfo>());
+        await ImportFromCsv(tagPath, csv => csv.GetRecords<Tag>());
 
         await _context.SaveChangesAsync();
     }
 
-    private void ImportFromCsv(string path)
+    private async Task ImportFromCsv<T>(string path, Func<CsvReader, IEnumerable<T>> getRecords) where T : class
     {
         using var reader = new StreamReader(path);
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 
-        var records = csv.GetRecords<Recipe>().ToList();
+        var records = getRecords(csv).ToList();
 
-        foreach (var record in records)
-        {
-            _context.Recipes.Add(record);
-        }
+        await _context.Set<T>().AddRangeAsync(records);
     }
 }
